1cf70e4c7ab4c4cf2f7be373de6a0629fd2a5d3b
sunghwki <swkim12345@naver.com>
2024-12-11 02:50:09 +0900

    ♻️ refactor(readfilesync -> readfile): sync가 있어 promises로 변경

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common';
-import { Book } from './entities/book.entity';
-import { Repository } from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
 import * as fs from 'fs';
 import * as path from 'path';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Book } from './entities/book.entity';
 
 @Injectable()
 export class BookService {
@@ -30,18 +30,36 @@ export class BookService {
     });
   }
 
+  async splitAndMergeByNewline(filename: string): Promise<string> {
+    const file = await fs.promises.readFile(filename, 'utf8');
+    return file.split('.').reduce((prev, cur) => {
+      prev += cur + '.\n\t';
+      return prev;
+    }, '');
+  }
+
   async findOne(id: number) {
     const book = await this.bookRepository.findOne({ where: { id } });
     const imagePath = path.join(book.image);
-    const image = fs.readFileSync(imagePath, 'base64');
-    const content = fs.readFileSync(book.detail, 'utf8').split(".").reduce((prev, cur) => {
-      prev += cur + ".\n\t";
-      return prev;
-    }, "");
-    const intro = fs.readFileSync(book.content, 'utf8').split(".").reduce((prev, cur) => {
-      prev += cur + ".\n\t";
-      return prev;
-    }, "");
+    //const image = fs.readFileSync(imagePath, 'base64');
+    //const content = fs
+    //  .readFileSync(book.detail, 'utf8')
+    //  .split('.')
+    //  .reduce((prev, cur) => {
+    //    prev += cur + '.\n\t';
+    //    return prev;
+    //  }, '');
+
+    //const intro = fs
+    //  .readFileSync(book.content, 'utf8')
+    //  .split('.')
+    //  .reduce((prev, cur) => {
+    //    prev += cur + '.\n\t';
+    //    return prev;
+    //  }, '');
+    const image = await fs.promises.readFile(imagePath, 'base64');
+    const content = await this.splitAndMergeByNewline(book.detail);
+    const intro = await this.splitAndMergeByNewline(book.content);
 
     return {
       id: book.id,
